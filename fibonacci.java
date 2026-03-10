70385faffe7aa2f9b4c4ff9e5114188f0048dc4b
Bhavya Sri <24r21a6767@mlrit.ac.in>
2026-03-10 21:48:04 +0530

    Added fibonacci series program

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci {
+    static int fibonacci(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args){
+        int n=7;
+        for(int i=0;i<n;i++){
+            System.out.println(fibonacci(i));
+        }
+    }
+}
