4e037e833a7b7af103566edec7ac67beca55b4cf
Bhavya Sri <24r21a6767@mlrit.ac.in>
2026-03-13 14:32:57 +0530

    Added reverse of an array

---
+++ reverse_of_array.java
@@ -0,0 +1,18 @@
+public class reverse_of_array {
+    static void reverse(int arr[],int left,int right){
+        if(left>right){
+            return;
+        }
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        reverse (arr,left+1,right-1);
+    }
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        reverse(arr,0,arr.length-1);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
